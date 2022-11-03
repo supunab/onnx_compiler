37008afe2f78321954038ea675fbffcee7ecb2a7
Supun Abeysinghe <supunabeysinghe@gmail.com>
2022-11-02 21:37:08 -0400

    minor changes to small case benchmarking

--- test/simple_model/constants.py
+++ test/simple_model/constants.py
@@ -1,10 +1,9 @@
 # setup data
 input_size = 28 * 28
 # TODO: for the moment, let's have fixed batch dim as well
-batch_size = 1024
-num_layers = 6
-hidden_sizes = [1024, 2048, 2048, 2048, 512, 10]
-
+batch_size = 1024 * 1
+num_layers = 8
+hidden_sizes = [1024, 2048, 4096, 2048, 1024, 512, 64, 10]
 
 # for small tests
 # input_size = 10
